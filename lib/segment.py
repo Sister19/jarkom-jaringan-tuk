244dcda75af36de68e91b8a91f67b88fb04f23ea
TubesForLyfe <13520160@std.stei.itb.ac.id>
2022-11-11 12:28:01 +0700

    Initialize Segment

--- lib/segment.py
+++ lib/segment.py
@@ -1,19 +1,19 @@
 import struct
 
 # Constants 
-SYN_FLAG = 0b0
-ACK_FLAG = 0b0
-FIN_FLAG = 0b0
+SYN_FLAG = 0b00000001
+ACK_FLAG = 0b00000100
+FIN_FLAG = 0b00000000
 
 class SegmentFlag:
     def __init__(self, flag : bytes):
         # Init flag variable from flag byte
+        self.flag = flag
         pass
 
     def get_flag_bytes(self) -> bytes:
         # Convert this object to flag in byte form
-        pass
-
+        return self.flag
 
 
 
@@ -22,12 +22,22 @@ class Segment:
     # -- Internal Function --
     def __init__(self):
         # Initalize segment
+        self.header = {}
+        self.flag = SegmentFlag(0b0)
+        self.checksum = 0
+        self.payload = 0b0
         pass
 
     def __str__(self):
         # Optional, override this method for easier print(segmentA)
         output = ""
-        output += f"{'Sequence number':24} | {self.sequence}\n"
+        seq_num = self.header["seq_num"]
+        ack_num = self.header["ack_num"]
+        output += f"{'Sequence number':24} | {seq_num}\n"
+        output += f"{'Acknowledge number':24} | {ack_num}\n"
+        output += f"{'Flag':24} | {self.flag}\n"
+        output += f"{'Checksum':24} | {self.checksum}\n"
+        output += f"{'Payload':24} | {self.payload}\n"
         return output
 
     def __calculate_checksum(self) -> int:
@@ -37,24 +47,27 @@ class Segment:
 
     # -- Setter --
     def set_header(self, header : dict):
+        self.header = header
         pass
 
     def set_payload(self, payload : bytes):
+        self.payload = payload
         pass
 
     def set_flag(self, flag_list : list):
+        self.flag = flag_list[0]
         pass
 
 
     # -- Getter --
     def get_flag(self) -> SegmentFlag:
-        pass
+        return self.flag
 
     def get_header(self) -> dict:
-        pass
+        return self.header
 
     def get_payload(self) -> bytes:
-        pass
+        return self.payload
 
 
     # -- Marshalling --
